63c84c45306b9d4227532fc55268aa44ca8b4910
Isaiah White <isaiahwhite208@gmail.com>
2018-08-16 15:05:40 -0600

    push

--- app/components/image/image-service.js
+++ app/components/image/image-service.js
@@ -4,14 +4,13 @@ const apiUrl = '//bcw-getter.herokuapp.com/?url=' + encodeURIComponent('http://w
 
 
 const imgApi = axios.create({
-	baseURL: '//bcw-getter.herokuapp.com/?url=' + encodeURIComponent('http://www.splashbase.co/api/v1/images/random'),
+	baseURL: apiUrl,
 	timeout: 3000
 });
 
 export default class ImageService {
 	getImage(background) {
 		// ^^^^^^^ How do you call this function?
-		console.log("Looking for a good pic")
 		imgApi().then(res => {
 			console.log('Image Data:', res.data)
 			background(res.data)
