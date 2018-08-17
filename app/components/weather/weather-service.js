f8eb537002371a9b08c0af2c08b73c3ccc2591d7
Isaiah White <isaiahwhite208@gmail.com>
2018-08-17 10:46:04 -0600

    push

--- app/components/weather/weather-service.js
+++ app/components/weather/weather-service.js
@@ -1,5 +1,5 @@
 const url = '//bcw-getter.herokuapp.com/?url=';
-const url2 = 'http://api.openweathermap.org/data/2.5/weather?q=boise&&APPID=bd82255fd0a21fa1238699b9eda2ee35'
+const url2 = 'http://api.openweathermap.org/data/2.5/weather?q=boise&&APPID=5dc1d5974319a52a7f4527f8594b7863'
 const apiUrl = url + encodeURIComponent(url2);
 
 const weatherApi = axios.create({
